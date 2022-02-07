c9ce4bdc8a6b9101e09042c8ea7a81622fc65bd8
Brett Davis <dbretta25@gmail.com>
2022-02-07 07:13:27 -0800

    made linear lasso reg modeling fxn

--- cotinine_regression_models/new_regression_functions.R
+++ cotinine_regression_models/new_regression_functions.R
@@ -1,91 +1,5 @@
 
-#---------------------------------------------------------#
-# FUNCTION                                                #
-# "gen_recipe"                                            #
-# recipe of pre-processing steps on dataset for modeling  #
-#---------------------------------------------------------#
 
-# for now, using this for all ML methods
-
-# recipe function to perform on datasets
-# include the prep step last
-gen_recipe <- function(dataset) {
-  recipe(Late_Cotinine_ng_ml ~ ., data=dataset) %>%
-    update_role(Sample_Name, new_role="ID") %>%
-    step_zv(all_numeric(), -all_outcomes()) %>%
-    step_normalize(all_numeric(), -all_outcomes())
-}
-
-# New execute modeling function without subsetting nCpGs (just use input CpGs)
-
-#-------------------------------------------------------------------#
-# FUNCTION                                                          #
-# "execute_modeling"                                                #
-# general function to perform modeling and assessment on a CV fold  #
-#-------------------------------------------------------------------#
-
-# This function calls several functions in order to perform modeling and
-#   assessment
-# INPUTS: splits: CV fold (my_folds$splits[[1]])
-#                id: my_folds$id, fold of the CV
-#                pd: metadata table object
-#              padj: for "find_assoc_features", padj cutoff for top CpGs to
-#                      return
-#                 n: for "find_assoc_features", number of top CpGs to return
-#            rm_cor: for "find_assoc_features", whether or not to remove
-#                      correlated top CpGs
-#        cor_cutoff: for "find_assoc_features", cutoff value to remove
-#                      correlated CpGs
-#          mod_spec: the model specification object
-#            method: ML method used, for proper variable importance collection
-#                      and export
-#                    one of ("penalized_logreg", "random_forest", "gbm")
-#                    penalized_logreg is used for lasso and elastic net
-# hyperparam_string: the string of hyperparam values to be used in file name
-#                      export
-#      pred_results: a tibble with the hyperparameters as columns
-
-execute_modeling <- function(splits, response_var, id, pd, padj=0.05, n=100, rm_cor=TRUE,
-  cor_cutoff=0.75, mod_spec, method, hyperparam_string, pred_results) {
-  # prepare the analysis cv split set to be used with limma
-  #limma_input_mat <- prep_for_limma(splits)
-
-  # get analysis set of CV split
-  df <- analysis(splits)
-  
-  # pre-process with recipe
-  analysis_prepped <- gen_recipe(df) %>%
-    prep(strings_as_factors = FALSE)
-
-  # bake the data
-  analysis_baked <- analysis_prepped %>%
-    bake(new_data=df
-
-  # fit the input model on pre-processed analysis set
-  # note that we need to exclude our ID variable (Sample_Name)
-  #   because we are not using a workflow()
-  # the fit function doesn't know that the ID column of the tibble has a
-  #   special role
-  mod_fit <- mod_spec %>%
-    fit(Late_Cotinine_ng_ml ~ ., data=analysis_baked[ ,!(colnames(analysis_baked) %in% "Sample_Name")])
-
-  # get assessment set of this split
-  assessment_set <- assessment(splits)
-  # pre-process the assessment set using the statistics computed from the
-  #   analysis set
-  # subset assessment set to include the same CpGs as used in the cleaned
-  #   analysis set
-  assessment_set_sub <- assessment_set[,colnames(assessment_set) %in%
-    colnames(df)]
-  # preprocess with same prepped data
-  assessment_baked <- analysis_prepped %>%
-    bake(new_data=assessment_set_sub)
-
-
-
-
-
-)
 
 
 # INPUT: colname: name of column in pd to be added to beta matrix (usually the response variable)
@@ -110,21 +24,161 @@ prep_input <- function(beta_matrix, pd, colname) {
 }
 
 
+#---------------------------------------------------------#
+# FUNCTION                                                #
+# "gen_recipe"                                            #
+# recipe of pre-processing steps on dataset for modeling  #
+#---------------------------------------------------------#
 
-# Function to prepare a cv split object (from rsample's vfold_cv) by prepping
-#   analysis set for Limma
-#   - remove columns: Sample_Name and status
-#   - transponse to have samples as columns and CpGs as rows
-prep_for_limma2 <- function(splits, colname) {
-  # obtain the data for a split from the analysis set
-  df <- analysis(splits)
+# for now, using this for all ML methods
 
-  # remove Sample_Name and status
-  df2 <- subset(df, select=-c(Sample_Name, Late_Cotinine_ng_ml))
-
-  # transpose
-  df_t <- as.data.frame(t(df2))
-
-  # return(df_t)
+# recipe function to perform on datasets
+# include the prep step last
+gen_recipe <- function(dataset) {
+  recipe(Late_Cotinine_ng_ml ~ ., data=dataset) %>%
+    update_role(Sample_Name, new_role="ID") %>%
+    step_zv(all_numeric(), -all_outcomes()) %>%
+    step_normalize(all_numeric(), -all_outcomes())
 }
 
+
+#-----------------------------------------------------------------------#
+# FUNCTION                                                              #
+# "penalized_logreg_var_imp"                                            #
+# function to obtain nonzero cpgs for LASSO or Elastic Net fitted model #
+#-----------------------------------------------------------------------#
+# INPUTS:        mod_fit: a fitted model object
+#      hyperparam_string: string with hyperparameters and values for naming exported file
+#                     id: my_folds$id, passed in via execute_modeling function argument
+penalized_logreg_var_imp <- function(mod_fit, hyperparam_string, id) {
+  # collect variable importance
+  imp_vars <- tidy(mod_fit)
+  # subset to features with nonzero coefficients
+  nonzero_imp_vars <- imp_vars[imp_vars$estimate != 0, ]
+
+  # grab intercept
+  my_int <- nonzero_imp_vars[1,]
+  # order by absolute value of coefficients
+  nonzero_imp_vars <- nonzero_imp_vars[-1,]
+  nonzero_imp_vars2 <- nonzero_imp_vars[order(abs(nonzero_imp_vars$estimate), decreasing=T), ]
+  # add intercept
+  nonzero_imp_vars <- rbind(my_int, nonzero_imp_vars2)
+
+  # export variable importance
+  write.table(as.data.frame(nonzero_imp_vars),
+    paste0(hyperparam_string, "_", id, "_nonzero_predictors.txt"),
+    sep="\t",
+    col.names=T,
+    row.names=F,
+    quote=F
+  )
+}
+
+
+# New execute modeling function without subsetting nCpGs (just use input CpGs)
+
+#-------------------------------------------------------------------#
+# FUNCTION                                                          #
+# "execute_modeling"                                                #
+# general function to perform modeling and assessment on a CV fold  #
+#-------------------------------------------------------------------#
+
+# This function calls several functions in order to perform modeling and
+#   assessment
+# INPUTS: splits: CV fold (my_folds$splits[[1]])
+#                id: my_folds$id, fold of the CV
+#                pd: metadata table object
+#          mod_spec: the model specification object
+#            method: ML method used, for proper variable importance collection
+#                      and export
+#                    one of ("penalized_logreg", "random_forest", "gbm")
+#                    penalized_logreg is used for lasso and elastic net
+# hyperparam_string: the string of hyperparam values to be used in file name
+#                      export
+#      pred_results: a tibble with the hyperparameters as columns
+
+execute_modeling <- function(splits, id, pd, mod_spec, method, hyperparam_string, pred_results) {
+  # prepare the analysis cv split set to be used with limma
+  #limma_input_mat <- prep_for_limma(splits)
+
+  # get analysis set of CV split
+  df <- analysis(splits)
+  
+  # pre-process with recipe
+  analysis_prepped <- gen_recipe(df) %>%
+    prep(strings_as_factors = FALSE)
+
+  # bake the data
+  analysis_baked <- analysis_prepped %>%
+    bake(new_data=df)
+
+  # fit the input model on pre-processed analysis set
+  # note that we need to exclude our ID variable (Sample_Name)
+  #   because we are not using a workflow()
+  # the fit function doesn't know that the ID column of the tibble has a
+  #   special role
+  mod_fit <- mod_spec %>%
+    fit(Late_Cotinine_ng_ml ~ .,
+    data=analysis_baked[ ,!(colnames(analysis_baked) %in% "Sample_Name")])
+
+  # get assessment set of this split
+  assessment_set <- assessment(splits)
+  # pre-process the assessment set using the statistics computed from the
+  #   analysis set
+  # subset assessment set to include the same CpGs as used in the cleaned
+  #   analysis set
+  assessment_set_sub <- assessment_set[,colnames(assessment_set) %in%
+    colnames(df)]
+  # preprocess with same prepped data
+  assessment_baked <- analysis_prepped %>%
+    bake(new_data=assessment_set_sub)
+
+  #----- Variable Importance -----#
+
+  print("collecting feature importance for penalized logistic regression model.")
+  penalized_logreg_var_imp(mod_fit=mod_fit,
+    hyperparam_string=hyperparam_string,
+    id=id
+  )
+  #----- Store Prediction Results -----#
+
+  # First repeat hyperparameter columns to have correct number of rows for
+  #   merging
+  rep_num <- length(assessment_baked$Sample_Name)
+  newtib <- pred_results %>%
+    dplyr::slice(rep(1:n(), each = rep_num))
+
+  # Add the prediction results to the input pred_results tibble
+  mod_results <- newtib %>%
+    add_column("id"=id) %>%
+    add_column("sample_name"=assessment_baked$Sample_Name) %>%
+    add_column("truth"=assessment_baked$Late_Cotinine_ng_ml) %>%
+    add_column("prediction"=mod_fit %>%
+      predict(new_data=assessment_baked) %>%
+        unlist())
+
+  #----- Store Performance Metrics -----#
+  my_mets <- tibble("rmse"=mod_results %>%
+                      metrics(truth, prediction) %>%
+                        filter(.metric == "rmse") %>%
+                          pull(.estimate),
+                    "rsq"=mod_results %>%
+                      metrics(truth, prediction) %>%
+                        filter(.metric == "rsq") %>%
+                          pull(.estimate),
+                    "mae"=mod_results %>%
+                      metrics(truth, prediction) %>%
+                        filter(.metric == "mae") %>%
+                          pull(.estimate)
+  )
+ 
+  #----- Combine Prediction Results and Metrics -----#
+
+  # add metrics to results tibble
+  my_res <- mod_results %>%
+    add_column(rmse=my_mets$rmse) %>%
+    add_column(rsq=my_mets$rsq) %>%
+    add_column(mae=my_mets$mae)
+
+  return(my_res)
+}
